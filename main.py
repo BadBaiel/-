4c647ce9fe0278c98622ce8c1929799873b5280d
BadBaiel <baiel.samurai@gmail.com>
2023-01-12 16:02:42 +0600

    hw1

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+
+bot = Bot(getenv('TG_TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands="start")
+async def echo(message: types.Message):
+    await message.answer(text=f'Привет! {message.from_user.first_name}, я бот! Решил тебя встретить.')
+    await message.delete()
+
+@dp.message_handler(commands="help")
+async def echo(message: types.Message):
+    await message.answer(text=f'Список команд /start-это начало беседы с ботом, /help-это список команд с коротким описанием, /myinfo-это информация о вас, /picture-это показ случайного фото.')
+    await message.delete()
+
+@dp.message_handler(commands="myinfo")
+async def echo(message: types.Message):
+    await message.answer(text=f'Ваш id-{message.from_user.id}, ваш nickname-{message.from_user.first_name}, ваш username-{message.from_user.username}')
+    await message.delete()
+
+# @dp.message_handler(commands=["picture"])
+# async def image_sender(message: types.Message):
+#     await message.answer_photo(
+#         open('./images/cat.webp', 'rb'),
+#     )
+#     await message.delete()
+@dp.message_handler(commands=['picture'])
+async def image_send(message: types.Message):
+    await message.answer_photo(
+        open('images/l5.jpg', 'rb')
+    )
+    await message.delete()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text.title())
+if __name__ == "__main__":
+    executor.start_polling(dp)
+
+
+
